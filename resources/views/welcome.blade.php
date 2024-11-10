0c53b814a7187b75950bfacfd5a1e24495dccedb
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-11-10 17:06:29 +0500

    deployed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <title>Your Laravel App</title>
   <title>Villa Agency - Real Estate HTML5 Template</title>
 
   <!-- Bootstrap core CSS -->
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'], // Update paths if needed
+      refresh: true,
+    }),
+  ],
+  assetsInclude: ['**/*.php'], // Add this line to treat PHP files as static assets
+});
